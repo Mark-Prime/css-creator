122e69163b9f2bb4e115d018521c3f4135e6f43b
Ryuk <msmarkieer@gmail.com>
2021-06-26 13:50:03 -0500

    Improved animations

--- src/Components/expander.js
+++ src/Components/expander.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const Open = keyframes`
+    from {
+        min-height: 0px;
+        max-height: 0px;
+        overflow-y: hidden;
+    }
+
+    to {
+        min-height: 10px;
+        max-height: 600px;
+        overflow-y: hidden;
+    }
+`
 
 const Content = styled.div`
     background: #303030;
     padding: 3px 0;
+    overflow-x: hidden;
+    overflow-y: auto;
+
+    min-height: 10px;
+    max-height: 600px;
+
+    animation: ${Open} .2s linear 0s 1 normal;
 `
 
 const Title = styled.div`
     cursor: pointer;
     padding: 1px 5px;
-    
+    transition: all .1s;
 
     &:hover {
         font-weight: bold;
+        margin-left: 5px;
     }
 `
 
@@ -28,6 +50,8 @@ class Expander extends Component {
             background: rgba(255,255,255, .12);
             margin-bottom: 3px;
             border-radius: 5px 0 0 ${this.state.open ? "0" : "5px"};
+            height: auto;
+            transition: all .05s;
 
             &:hover {
                 border-radius: 0px;
@@ -36,16 +60,20 @@ class Expander extends Component {
 
         const Notch = styled.div`
             float: left;
-            ${this.state.open ? "transform: rotate(90deg) translateY(-1rem) translateX(.3rem);"
-            : "transform: rotate(0deg) translateY(-.7rem);"}
+            transform: rotate(0deg) translateY(-.7rem);
             margin-right: 10px;
             font-size: 1.6rem;
             height: .6rem;
+            transition: all .1s;
+
+            &[open] {
+                transform: rotate(90deg) translateY(-1rem) translateX(.3rem);
+            }
         `
         return ( 
             <Wrapper>
                 <Title onClick={this.ToggleOpen}>
-                    <Notch>&#8250;</Notch>
+                    <Notch open={this.state.open}>&#8250;</Notch>
                     {this.props.title}
                 </Title>
                 
--- src/properties/textProperties.js
+++ src/properties/textProperties.js
@@ -13,42 +13,42 @@ const TextBox = styled.input`
 `
 
 const InputLabel = styled.label`
-
+    padding-left: 4px;
 `
 
 class TextProperties extends Component {
     render() { 
         return ( 
             <Expander title="Text">
-                    <Input>
-                        <InputLabel>Content: </InputLabel>
-                        <TextBox value={this.props.parentProps.text} onChange={this.props.parentProps.setText}></TextBox>
-                    </Input>
-                    <hr />
-                    <NumberSelect 
-                        name="fontSize" 
-                        style={this.props.parentProps.style} 
-                        enabled={this.props.parentProps.enabled} 
-                        OnStyleChange={this.props.OnStyleChange} 
-                        toggleEnabled={this.props.parentProps.toggleEnabled} 
-                    />
-                    <Select 
-                        name="textAlign" 
-                        options={['center', 'left', 'right', 'justify']}
-                        style={this.props.parentProps.style} 
-                        enabled={this.props.parentProps.enabled} 
-                        OnStyleChange={this.OnStyleChange} 
-                        toggleEnabled={this.props.parentProps.toggleEnabled}
-                    />
-                    <Select 
-                        name="textDecoration" 
-                        options={['none', 'overline', 'line-through', 'underline']}
-                        style={this.props.parentProps.style} 
-                        enabled={this.props.parentProps.enabled} 
-                        OnStyleChange={this.OnStyleChange} 
-                        toggleEnabled={this.props.parentProps.toggleEnabled}
-                    />
-                </Expander>
+                <Input>
+                    <InputLabel>Content: </InputLabel>
+                    <TextBox value={this.props.parentProps.text} onChange={this.props.parentProps.setText}></TextBox>
+                </Input>
+                <hr />
+                <NumberSelect 
+                    name="fontSize" 
+                    style={this.props.parentProps.style} 
+                    enabled={this.props.parentProps.enabled} 
+                    OnStyleChange={this.props.OnStyleChange} 
+                    toggleEnabled={this.props.parentProps.toggleEnabled} 
+                />
+                <Select 
+                    name="textAlign" 
+                    options={['center', 'left', 'right', 'justify']}
+                    style={this.props.parentProps.style} 
+                    enabled={this.props.parentProps.enabled} 
+                    OnStyleChange={this.OnStyleChange} 
+                    toggleEnabled={this.props.parentProps.toggleEnabled}
+                />
+                <Select 
+                    name="textDecoration" 
+                    options={['none', 'overline', 'line-through', 'underline']}
+                    style={this.props.parentProps.style} 
+                    enabled={this.props.parentProps.enabled} 
+                    OnStyleChange={this.OnStyleChange} 
+                    toggleEnabled={this.props.parentProps.toggleEnabled}
+                />
+            </Expander>
          );
     }
 }
