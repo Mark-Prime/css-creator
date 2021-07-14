7bdecfbed84d280048cfc85e340315dd1d0c520f
Ryuk <msmarkieer@gmail.com>
2021-07-14 12:15:45 -0500

    added placeholder editSelect

---
+++ src/Layout/editSelect.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.form`
+    padding: 5px 0;
+    border-top: 2px rgba(255, 255, 255, 0.12) solid;
+    background: #303030;
+`
+
+const Input = styled.input`
+    display: none;
+
+    :checked + label {
+        border: 1px solid #ccc;
+        border-left: 2px solid #ccc;
+        border-right: 2px solid #ccc;
+        margin: 0;
+    }
+`
+
+const Label = styled.label`
+    font-size: 12px;
+    margin: 0 1px;
+    padding: 1px 4px 3px;
+    background-color: rgba(255, 255, 255, 0.12);
+    border: 1px solid rgba(0, 0, 0, .7);
+`
+
+const Group = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+`
+
+class EditSelect extends Component {
+    state = { 
+        selected: 'button'
+     }
+    render() { 
+        return ( 
+            <Wrapper>
+                <Group>
+                    <Input type="radio" id='container' name='editSelect' value='container' />
+                    <Label for='container'>Container</Label>
+                    <Input type="radio" id='button' name='editSelect' value='button' defaultChecked={true} />
+                    <Label for='button'>Button</Label>
+                    <Input type="radio" id=':hover' name='editSelect' value=':hover' />
+                    <Label for=':hover'>:hover</Label>
+                    <Input type="radio" id=':active' name='editSelect' value=':active' />
+                    <Label for=':active'>:active</Label>
+                    <Input type="radio" id=':focus' name='editSelect' value=':focus' />
+                    <Label for=':focus'>:focus</Label>
+                    <Input type="radio" id=':target' name='editSelect' value=':target' />
+                    <Label for=':target'>:target</Label>
+                </Group>
+            </Wrapper>
+         );
+    }
+}
+ 
+export default EditSelect;
--- src/Layout/properties.js
+++ src/Layout/properties.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import Panel from '../properties/propertyPanel';
+import EditSelect from './editSelect';
 import { connect } from 'react-redux';
 
 const Wrapper = styled.div`
     background: #303030;
     border-left: 2px rgba(255, 255, 255, 0.12) solid;
     width: 300px;
+    height: 100vh; 
+    overflow-x: hidden;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+const PropList = styled.div`
+    flex-shrink: 1;
     padding: 3px;
     overflow-x: hidden;
     overflow-y: auto;
-`
+`;
 
 const Input = styled.div`
     display: flex;
@@ -29,16 +39,19 @@ const mapStateToProps = ({ styles }) => ({ styles });
 export default connect(mapStateToProps)(function Properties(props) {
     return ( 
         <Wrapper>
-            <Input>
-                <InputLabel>Content: </InputLabel>
-                <TextBox value={props.text} onChange={props.setText}></TextBox>
-            </Input>
-            <hr />
-            {Object.keys(props.styles).map((key) => {
-                return (
-                    <Panel styles={props.styles[key].props} title={key}/>
-                )
-            })}
+            <PropList>
+                <Input>
+                    <InputLabel>Content: </InputLabel>
+                    <TextBox value={props.text} onChange={props.setText}></TextBox>
+                </Input>
+                <hr />
+                {Object.keys(props.styles).map((key) => {
+                    return (
+                        <Panel styles={props.styles[key].props} title={key}/>
+                    )
+                })}
+            </PropList>
+            <EditSelect />
         </Wrapper>
         );
 })
