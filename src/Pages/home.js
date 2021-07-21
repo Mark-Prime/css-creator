0558afb74bda5e339db244850955108eade2f10f
Mark-Prime <msmarkieer@gmail.com>
2021-07-21 12:16:17 -0500

    Added scroll through descriptors animation

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import CodingBackground from '../imgs/coding-mid.jpg'
 
 const Container = styled.div`
@@ -17,27 +17,68 @@ const Header = styled.div`
     height: 90vh;
 `
 
-const Adjective = styled.div`
-    font-family: Inter, sans-serif;
-    color: white;
-    font-size: 3rem;
-    display: inline-block;
+const animation = keyframes`
+    0% {
+        transform: translateY(-120px);
+    }
 
-    &::before {
-        content: 'I AM ';
+    10% {
+        transform: translateY(-120px);
+    }
+
+    30% {
+        transform: translateY(-300px);
+    }
+
+    40% {
+        transform: translateY(-300px);
+    }
+
+    60% {
+        transform: translateY(0);
+    }
+
+    70% {
+        transform: translateY(0);
+    }
+
+    100% {
+        transform: translateY(calc(-100% + 3.7rem));
     }
 `
 
+const Descriptor = styled.div`
+    font-family: Inter, sans-serif;
+    color: white;
+    font-size: 3rem;
+    text-align: left;
+    position: absolute;
+    left: 50%;
+    height: fit-content;
+    transform: translateY(calc(-100% + 3.7rem));
+    animation: ${animation} 2.5s ease;
+    padding: 0;
+    margin: 0;
+`
+
+const IAM = styled.div`
+    position: absolute;
+    right: 51%;
+`
+
 const SubHeader = styled.div`
+    position: relative;
     text-align: center;
     transition: all 0.1s ease-out;
     cursor: default;
-
-    &:hover {
-        font-weight: 700;
-        transform: scale(1.1) translateY(-10px);
-        text-shadow: 0 10px 5px #000;
-    }
+    font-family: Inter, sans-serif;
+    padding-top: 10vh;
+    overflow: hidden;
+    color: white;
+    font-size: 3rem;
+    height: 20vh;
+    width: 100%;
+    mask: linear-gradient(0deg, rgba(255,255,255,0) 10%, rgba(0,0,0,1) 45%, rgba(0,0,0,1) 55%, rgba(255,255,255,0) 90%);
 `
 
 const NamePlate = styled.div`
@@ -45,20 +86,21 @@ const NamePlate = styled.div`
     font-size: 3rem;
     text-align: center;
     padding-top: 10vh;
-    margin-bottom: 10vh;
     font-weight: 700;
 `
 
-const adjectives = ['a software engineer', 'a photographer', 'a video editor', 'a front-end developer', 'an artist', 'a gamer', 'ME'];
+const descriptors = ['a software engineer', 'a movie lover', 'a front-end developer', 'a video editor', 'a photographer', 'an artist', 'a gamer'];
+
 
 class Home extends Component {
+    state = { index: 0 }
+
     render() { 
         return ( 
             <Container>
                 <Header>
                     <NamePlate>Mark Spannbauer</NamePlate>
-
-                    <SubHeader><Adjective>ME</Adjective></SubHeader>
+                    <SubHeader><IAM>I AM </IAM><Descriptor>{descriptors.map((descriptor) => (<>{descriptor}<br/></>))}<br/><br/>ME</Descriptor></SubHeader>
                 </Header>
             </Container>
          );
