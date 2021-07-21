a34d591704f77ae777248e14b4a1c66a74b3278f
Mark-Prime <msmarkieer@gmail.com>
2021-07-21 10:59:53 -0500

    Added home page styling

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import CodingBackground from '../../public/img/coding-mid.jpg'
+import CodingBackground from '../imgs/coding-mid.jpg'
 
 const Container = styled.div`
     height: 100%;
@@ -9,22 +9,56 @@ const Container = styled.div`
 `
 
 const Header = styled.div`
-    background-image: url(${CodingBackground});
+    font-family: Montserrat, sans-serif;
+    background-image: linear-gradient(to right, rgba(0, 0, 0, 0.2), rgba(255, 255, 255, 0.2)), url(${CodingBackground});
+    background-repeat: no-repeat;
+    background-position: center center;
     width: 100%;
     height: 90vh;
 `
 
-const NamePlate = styled.div`
+const Adjective = styled.div`
+    font-family: Inter, sans-serif;
+    color: white;
+    font-size: 3rem;
+    padding-top: 10vh;
+    display: inline-block;
 
+    &::before {
+        content: 'I AM ';
+    }
 `
 
+const SubHeader = styled.div`
+    text-align: center;
+    transition: all 0.1s ease-out;
+    cursor: default;
+
+    &:hover {
+        font-weight: 700;
+        transform: scale(1.1) translateY(-10px);
+        text-shadow: 0 10px 5px #000;
+    }
+`
+
+const NamePlate = styled.div`
+    color: #fff;
+    font-size: 3rem;
+    text-align: center;
+    padding-top: 10vh;
+    font-weight: 700;
+`
+
+const adjectives = ['a software engineer', 'a photographer', 'a video editor', 'a front-end developer', 'an artist', 'a gamer', 'ME'];
+
 class Home extends Component {
-    state = {  }
     render() { 
         return ( 
             <Container>
                 <Header>
                     <NamePlate>Mark Spannbauer</NamePlate>
+
+                    <SubHeader><Adjective>ME</Adjective></SubHeader>
                 </Header>
             </Container>
          );
