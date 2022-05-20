1bbc44fa03c2af22f970322979b7f690c5595712
Mark-Prime <msmarkieer@gmail.com>
2022-05-19 21:38:40 -0500

    Updated the animations

--- src/Components/Cyberpunk/Card.js
+++ src/Components/Cyberpunk/Card.js
@@ -10,7 +10,7 @@ let colorPrimary = "#fe3218"
 
 const Front = styled.div`
     background: black;
-    border: 2px solid ${colorPrimary};
+    border: 1px solid ${colorPrimary};
     width: 100%;
     height: 100%;
     position: absolute;
@@ -24,20 +24,17 @@ const Front = styled.div`
         calc(100% - 5rem) 100%,
         0 100%
         );
-
-    transition: all .3s;
 `
 
 const Back = styled.div`
     background: ${colorHighlight};
-    border: 2px solid ${colorHighlight};
+    border: 1px solid ${colorHighlight};
     width: 100%;
     height: 100%;
     position: absolute;
     top: 0;
     left: 0;
     z-index: 1;
-    transition: all .3s;
 
     clip-path: polygon(
         100% 0,
@@ -48,9 +45,23 @@ const Back = styled.div`
         );
 `
 
+const Title = styled.h1`
+    font-size: 2.5rem;
+    text-align: center;
+    color: ${colorHighlight};
+    transition: all .3s;
+
+    font-family: 'inter', sans-serif;
+    font-weight: 100;
+`
+
 const Container = styled.div`
     position: relative;
     z-index: 1;
+
+    & * {
+        transition: all .3s;
+    }
 
     &:hover ${Front} {
         background: ${colorPrimary};
@@ -73,12 +84,10 @@ const Container = styled.div`
             100% 100%
             );
     }
-`
 
-const Title = styled.h1`
-    font-size: 2.5rem;
-    text-align: center;
-    color: ${colorHighlight};
+    &:hover ${Front} > * {
+        opacity: 0;
+    }
 `
 
 function Card(props) {
